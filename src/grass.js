0643e03188edf1cde50442d05df30bc4fa1405bb
Philip Scott <pscott@zeptohost.com>
2017-11-04 19:10:26 -0400

    Add grass module

---
+++ src/grass.js
@@ -0,0 +1,53 @@
+// grass.js
+// given a scene, appends additional meshes and materials, then
+// returns a tuple, the new scene + function to be called during render
+
+var THREE = require('three')
+
+function generateTexture() {
+  var canvas = document.createElement( 'canvas' );
+  canvas.width = 512;
+  canvas.height = 512;
+  var context = canvas.getContext( '2d' );
+  for ( var i = 0; i < 20000; i ++ ) {
+    context.fillStyle = 'hsl(0,0%,' + ( Math.random() * 50 + 50 ) + '%)';
+    context.beginPath();
+    context.arc( Math.random() * canvas.width, Math.random() * canvas.height, Math.random() + 0.15, 0, Math.PI * 2, true );
+    context.fill();
+  }
+  context.globalAlpha = 0.075;
+  context.globalCompositeOperation = 'lighter';
+  return canvas;
+}
+
+function addGrass (scene) {
+  var geometry = new THREE.PlaneBufferGeometry( 100, 100 );
+  var texture = new THREE.CanvasTexture( generateTexture() );
+  var grassHeight = 20
+  for ( var i = 0; i < grassHeight; i ++ ) {
+    var material = new THREE.MeshBasicMaterial( {
+      color: new THREE.Color().setHSL( 0.3, 0.75, ( i / 15 ) * 0.4 + 0.1 ),
+      map: texture,
+      depthTest: false,
+      depthWrite: false,
+      transparent: true
+    } );
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.position.y = i * 0.25;
+    mesh.rotation.x = - Math.PI / 2;
+    scene.add( mesh );
+  }
+
+  function grassRender () {
+    var time = Date.now() / 6000;
+    for ( var i = 0; i < grassHeight; i ++ ) {
+      var mesh = scene.children[ i ];
+      mesh.position.x = Math.sin( time * 4 ) * i * i * 0.005;
+      mesh.position.z = Math.cos( time * 6 ) * i * i * 0.005;
+    }
+  }
+
+  return grassRender
+}
+
+module.exports = addGrass
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 var THREE = require('three')
+var addGrass = require('./grass')
 
 var scene = new THREE.Scene()
 scene.background = new THREE.Color(0x6aa2fc)
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-
+var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000)
+camera.position.set(0, 75, 100)
 var renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
@@ -12,13 +13,13 @@ var geometry = new THREE.BoxGeometry(1, 1, 1)
 var material = new THREE.MeshNormalMaterial({ color: 0x00ff00 })
 var cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
-
-camera.position.z = 3
+var grassRender = addGrass(cube)
 
 function animate () {
   cube.rotation.x += 0.01
   cube.rotation.y += 0.03
   requestAnimationFrame(animate)
+  grassRender()
   renderer.render(scene, camera)
 }
 animate()
