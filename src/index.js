f11c748e607f4071b32bd4ffcf0386900fb6a084
Philip Scott <pscott@zeptohost.com>
2017-11-04 17:46:57 -0400

    Purdy normal material

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var THREE = require('three')
 
 var scene = new THREE.Scene()
+scene.background = new THREE.Color( 0x6aa2fc )
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 var renderer = new THREE.WebGLRenderer()
@@ -8,7 +9,7 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 var geometry = new THREE.BoxGeometry(1, 1, 1)
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+var material = new THREE.MeshNormalMaterial({ color: 0x00ff00 })
 var cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
