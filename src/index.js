e733f368503820d6e27361a79d2ad40cfc90f81d
Sokojoe <sokojoey@gmail.com>
2017-11-04 19:38:07 -0400

    Fixed Camera rotatio

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var addGrass = require('./grass')
 var scene = new THREE.Scene()
 scene.background = new THREE.Color(0x6aa2fc)
 var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000)
-camera.position.set(0, 75, 100)
+camera.position.set(0, 55, 100)
 var renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
@@ -16,8 +16,8 @@ scene.add(cube)
 var grassRender = addGrass(cube)
 
 function animate () {
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.03
+  //cube.rotation.x += 0.01
+  //cube.rotation.y += 0.03
   requestAnimationFrame(animate)
   grassRender()
   renderer.render(scene, camera)
